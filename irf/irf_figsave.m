09eef043bb544ec6296815baa07f927670bcac6b
Ajay Lotekar <ablotekar@gmail.com>
2022-02-25 14:38:32 +0100

    change inputs

--- irf/irf_figsave.m
+++ irf/irf_figsave.m
@@ -1,51 +1,11 @@
-function irf_figsave(varargin) %fname,resol,frm, ext)
+function irf_figsave(fname,resol,frm, ext)
 %% Save figure in .jpg, .png, .eps and .pdf format 
 %
-% Input: fname = figure name
-%        res = Resolution (default 300 dpi)
-%        frem = frame size (default [12 6]
-%        ext  = file extention (.png, .jpg, .pdf, .eps) (default .png)
 %
 % Example:
-%       irf_figsave('figure01', 'res', 300, 'frem', [12 6], 'ext', '.png')
-%       irf_figsave('figure01')
+%       irf_figsave('figure01', 300, [12 6], '.png')
 %
-%  **** Not ready for use ******
-% Do do (1) fixed frame size input problem
-%%
-args = varargin;
-nargs = nargin;
-orignal_args=args;
-
-if nargs == 0 % show only help
-    help irf_subplot;
-    return
-end
-
-if nargs<2 || ~(mod(nargs,7)==0)
-    error('IRFU_MATLAB:irf_figsave:InvalidNumberOfInputs','Incorrect number of input arguments')
-end
-
-%%%-------- Default Values ----------------
-frm = [12 6];
-resol = 300;
-ext = '.png';
-
-fname = args{1};     % file name
-
-while ~isempty(args)
-    x = args{1}; args(1) = [];
-    disp(x)
-    switch lower(x)
-        case {'res'}
-            resol = args{1}; args(1) = [];
-        case {'frem'} 
-            frm = args{1}; args(1) = [];
-        case {'ext'}
-            ext= args{1}; args(1) = [];
-    end
-end
-
+% Author : Ajay Lotekar
 
 set(gcf,'PaperUnits','inches');
 set(gcf,'PaperSize',[frm(1) frm(2)]);
